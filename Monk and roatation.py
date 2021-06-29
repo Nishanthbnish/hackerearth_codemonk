5379d7c8488a79c2a5fa7cbf619f591252c60a5b
Nishant Bangera <43651531+Nishanthbnish@users.noreply.github.com>
2021-06-29 11:47:38 +0530

    Update Monk and roatation.py

--- Monk and roatation.py
+++ Monk and roatation.py
@@ -1,41 +1,6 @@
-class RotateArray {
-    /*Function to left rotate arr[] of size n by d*/
-    void leftRotate(int arr[], int d, int n)
-    {
-        for (int i = 0; i < d; i++)
-            leftRotatebyOne(arr, n);
-    }
- 
-    void leftRotatebyOne(int arr[], int n)
-    {
-        int i, temp;
-        temp = arr[0];
-        for (i = 0; i < n - 1; i++)
-            arr[i] = arr[i + 1];
-        arr[n-1] = temp;
-    }
- 
-    /* utility function to print an array */
-    void printArray(int arr[], int n)
-    {
-        for (int i = 0; i < n; i++)
-            System.out.print(arr[i] + " ");
-    }
- 
-    // Driver program to test above functions
-    public static void main(String[] args)
-    {
-        Scanner s=new Scanner();
-        RotateArray rotate = new RotateArray();
-        int n=5;
-        int arr[] ;
-        System.out.println("Enter the number")
-        s=sc.nextInt();
-        for(int i=0;i<=s;i++)
-        {
-            s
-        }
-        rotate.leftRotate(arr, 3, 5);
-        rotate.printArray(arr, 5);
-    }
-}
+testCase  = int(input())
+for _ in range(testCase):
+    n,k = map(int,input().split())
+    l = list(map(int,input().split()))
+    x = k%n
+    print(*(l[n-x:]+l[:n-x]))
